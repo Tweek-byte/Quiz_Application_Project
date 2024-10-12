69850df2fe81704035639f1f91e046f603702fc9
Tweek-Byte <zaka4396@gmail.com>
2024-10-12 18:44:00 +0100

    feat: Create quiz app

--- Quizora/Quizora/settings.py
+++ Quizora/Quizora/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'session',
+    'quiz',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
