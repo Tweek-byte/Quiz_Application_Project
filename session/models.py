ec1b91c00d3510a46d5602e7dfd3d0a2c6e9a98c
Tweek-Byte <zaka4396@gmail.com>
2024-10-14 12:20:55 +0100

    chore: linking Profile with Django admin system

--- session/models.py
+++ session/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     """Profile Database logic"""
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, verbose_name='User Obj')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, verbose_name='User_obj')
     
     profile_img = models.ImageField(
         upload_to='profile_images', 
         default='user.svg', 
         blank=True, 
         null=True, 
-        verbose_name='user_pic'
+        verbose_name='User_pic'
     )
 
     email_adress = models.CharField(max_length=55, unique=True, null=True)
