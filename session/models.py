32ac484eef4f20fbccb8bdca643d53aa6d5f5fb9
Tweek-Byte <zaka4396@gmail.com>
2024-10-14 11:46:29 +0100

    feat: create User Profile database

--- session/models.py
+++ session/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    """Profile Database logic"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, verbose_name='User Obj')
+    
+    profile_img = models.ImageField(
+        upload_to='profile_images', 
+        default='user.svg', 
+        blank=True, 
+        null=True, 
+        verbose_name='user_pic'
+    )
+
+    email_adress = models.CharField(max_length=55, unique=True, null=True)
+
+    GENDER = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    )
+
+    gender = models.CharField(max_length=17, choices=GENDER, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
