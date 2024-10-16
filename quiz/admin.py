e437088bbfa3efee7befca22ad356aef12921c8f
Tweek-Byte <zaka4396@gmail.com>
2024-10-16 22:16:18 +0100

    feat: registering database in admin system

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,9 +1,11 @@
 """Administration Panel Modems register"""
 from django.contrib import admin
-from .models import Category, Quiz, Question, Choice
+from .models import Category, Quiz, Question, Choice, QuizCompleted, ProfileRank
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(QuizCompleted)
+admin.site.register(ProfileRank)
