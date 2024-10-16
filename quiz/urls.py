fd5612de1be349727ffd52cec9c51211c66566f8
Tweek-Byte <zaka4396@gmail.com>
2024-10-17 00:29:44 +0100

    feat: implement taking quiz and showing results

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.quizzes_list, name='quizzes_list'),
     path('take', views.quiz_page, name='quiz_page'),
     path('search/<str:category>', views.search, name='search'),
+    path('<int:quiz_id>/', views.quiz_page, name='quiz'),
+    path('result/<int:submission_id>/', views.quiz_result, name='quiz_result')
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Quiz, Category, QuizCompleted
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-import pandas as p
 
 @login_required
 def quizzes_list(request):
-    """Logic for showing list of quizzes"""
+    """Logic for showing list of quizzes."""
     quizzes = Quiz.objects.order_by('-created_at')
     categories = Category.objects.all()
 
@@ -15,36 +14,44 @@ def quizzes_list(request):
 
 @login_required
 def search(request, category):
-    """Search quizzes logic"""
-    if request.GET.get('q') != None:
-        q = request.GET.get('q')
+    """Search quizzes logic."""
+    q = request.GET.get('q')
+    
+    if q:
         query = Q(name__icontains=q) | Q(description__icontains=q)
         quizzes = Quiz.objects.filter(query).order_by('-created_at')
-
     elif category != " ":
         quizzes = Quiz.objects.filter(category__name=category).order_by('-created_at')
-    
     else:
         quizzes = Quiz.objects.order_by('-created_at')
 
-
     categories = Category.objects.all()
-
     context = {"quizzes": quizzes, "categories": categories}
     return render(request, 'quizzes_list.html', context)
 
 @login_required
 def quiz_page(request, quiz_id):
-    """quiz page logic"""
-    qv = get_object_or_404(Quiz, pk=quiz_id)
+    """Logic for displaying a quiz page."""
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
 
     if request.method == "POST":
-
         score = int(request.POST.get('score', 0))
-
-        submission = QuizCompleted(user=request.user, quiz=qv, score=score)
+        submission = QuizCompleted(user=request.user, quiz=quiz, score=score)
         submission.save()
+        return redirect('quiz_result', submission_id=submission.id)
 
-        return redirect('quiz_result.html', submission_id=submission.id)
+    return render(request, 'quiz_page.html', {'quiz': quiz})
 
-    return (render(request, 'quiz_page.html', {'quiz': qv}))
+@login_required
+def quiz_result(request, submission_id):
+    """View to display quiz results."""
+    submission = get_object_or_404(QuizCompleted, id=submission_id)
+    total_questions = submission.quiz.questions.count()
+
+    context = {
+        'submission': submission,
+        'total_questions': total_questions,
+        'correct_answers': submission.score,
+        'incorrect_answers': total_questions - submission.score
+    }
+    return render(request, 'quiz_result.html', context)
