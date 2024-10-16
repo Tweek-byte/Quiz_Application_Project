a4f7026b43fb1a4c71af42ea124735cef0e14817
Tweek-Byte <zaka4396@gmail.com>
2024-10-16 17:05:28 +0100

    feat: implementing search function

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('quizzes_list', views.quizzes_list, name='quizzes_list'),
+    path('', views.quizzes_list, name='quizzes_list'),
     path('search/<str:category>', views.search, name='search'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Quiz, Category
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from session.models import Profile
+from django.db.models import Q
 
 @login_required
 def quizzes_list(request):
@@ -12,9 +15,21 @@ def quizzes_list(request):
     return render(request, 'quizzes_list.html', context)
 
 @login_required
-def search(request):
-    """Logic for quizzes list search function"""
-    context={
-        
-    }
-    return (render(request, 'quizzes_list.html', context))
+def search(request, category):
+    """Search quizzes logic"""
+    if request.GET.get('q') != None:
+        q = request.GET.get('q')
+        query = Q(name__icontains=q) | Q(description__icontains=q)
+        quizzes = Quiz.objects.filter(query).order_by('-created_at')
+
+    elif category != " ":
+        quizzes = Quiz.objects.filter(category__name=category).order_by('-created_at')
+    
+    else:
+        quizzes = Quiz.objects.order_by('-created_at')
+
+
+    categories = Category.objects.all()
+
+    context = {"quizzes": quizzes, "categories": categories}
+    return render(request, 'quizzes_list.html', context)
