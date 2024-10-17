d40aa830fda0ef663cb16999d44a1df968833c98
Tweek-Byte <zaka4396@gmail.com>
2024-10-17 09:10:52 +0100

    ops: edited database entries

--- Quizora/settings.py
+++ Quizora/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Quizora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '<database_name>',
+        'USER': '<username>',
+        'PASSWORD': '<password>',
+        'HOST': '<hostname>',
+        'PORT': '5432',
     }
 }
 
